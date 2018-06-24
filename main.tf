cc0e8bce71076997bf6cea14d11f8cfe9e5483a3
Daniil Kupchenko <kupchenko@gmail.com>
2018-06-24 23:31:23 +0300

    additional ports for proxy

--- main.tf
+++ main.tf
@@ -87,6 +87,20 @@ resource "aws_security_group" "tf-sg" {
         ipv6_cidr_blocks = ["::/0"]
     }
 
+    ingress {
+        from_port = 7034
+        to_port = 7034
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 7035
+        to_port = 7035
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
       from_port = 0
       to_port = 0
